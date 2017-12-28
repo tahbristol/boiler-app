801e09fdf80310f4c187ce757cde075607843adf
tahbristol <tahbristol@gmail.com>
2017-12-27 23:30:06 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Layout from './components/Layout'
+
+class App extends Component {
+  render() {
+    return (
+      <h1>It works</h1>
+    );
+  }
+}
+
+export default App;
